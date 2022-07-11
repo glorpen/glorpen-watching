cd8fd8324173459cef86e176b79120bf9d3b1814
Arkadiusz Dzięgiel <arkadiusz.dziegiel@glorpen.pl>
2022-07-11 11:38:31 +0200

    cron: suport week names, add command help

--- src/glorpen/watching/cron.py
+++ src/glorpen/watching/cron.py
@@ -1,6 +1,8 @@
 import argparse
+import itertools
 import logging
 import os
+import textwrap
 import time
 
 import more_itertools
@@ -11,10 +13,17 @@ from glorpen.watching import app
 
 console = logging.root.getChild("glorpen.watching.cron")
 
-time_parts = pyparsing.Or(
-    [pyparsing.Literal(part) for part in
-     more_itertools.flatten((range, f"{range}s") for range in ("hour", "day", "week", "minute", "second"))
-     ]
+weekdays = (
+    "monday",
+    "tuesday",
+    "wednesday",
+    "thursday",
+    "friday",
+    "saturday",
+    "sunday",
+)
+time_units = tuple(
+    more_itertools.flatten((range, f"{range}s") for range in ("hour", "day", "week", "minute", "second"))
 )
 
 specific_time = pyparsing.Literal("at") + pyparsing.Regex(r':?\d+(:\d+)*')
@@ -23,14 +32,14 @@ command = pyparsing.Literal(
     "do"
 ) + pyparsing.OneOrMore(
     pyparsing.Or(
-        [pyparsing.Regex(r"-[vq]+"), *(pyparsing.Literal(f"--{option}") for option in
-                                       ("pending", "completed", "setup", "ongoing", "verbose", "quiet", "config"))]
+        [pyparsing.Regex(r"-[vq]+")] + [pyparsing.Literal(f"--{option}") for option in
+                                        ("pending", "completed", "setup", "ongoing", "verbose", "quiet", "config")]
     )
 )
 
 grammar = pyparsing.Literal("every") + pyparsing.Optional(
-    pyparsing.Word(pyparsing.nums), default=1
-) + time_parts + pyparsing.Optional(specific_time) + command
+    pyparsing.Or(itertools.chain([pyparsing.Word(pyparsing.nums)], map(pyparsing.Literal, weekdays))), default=1
+) + pyparsing.Or(map(pyparsing.Literal, time_units)) + pyparsing.Optional(specific_time) + command
 
 
 def run_job(name, args):
@@ -40,24 +49,47 @@ def run_job(name, args):
 
 
 def schedule_job(name: str, config_line: str):
-    tokens = grammar.parse_string(config_line, parse_all=True)
+    tokens = list(reversed(grammar.parse_string(config_line, parse_all=True)))
+    job: schedule.Job
 
-    pos = 1
-    job: schedule.Job = getattr(schedule.every(tokens[pos]), tokens[pos + 1])
-    pos += 2
-    if tokens[3] == 'at':
-        job = getattr(job, 'at')(tokens[4])
-        pos += 2
+    assert tokens.pop() == "every"
 
-    args = tokens[pos + 1:]
-    job.do(run_job, name=name, args=args)
+    token = tokens.pop()
+    if token in weekdays:
+        job = getattr(schedule.every(), token)
+    else:
+        interval = int(token)
+        token = tokens.pop()
+        assert token in time_units
+        job = getattr(schedule.every(interval), token)
+
+    token = tokens.pop()
+    if token == "at":
+        job = job.at(tokens.pop())
+        token = tokens.pop()
+
+    assert token == "do"
+    job.do(run_job, name=name, args=tokens)
 
     console.info(f"Registered {name} to run on {job.next_run}")
 
 
 if __name__ == "__main__":
 
-    p = argparse.ArgumentParser()
+    p = argparse.ArgumentParser(
+        formatter_class=argparse.RawDescriptionHelpFormatter, description=textwrap.dedent(
+            """\
+                To schedule gwatching runs, define some env variables, for eg.:
+                JOB_hourly="every 1 hour at 00:34 do --pending -qqq"
+                JOB_weekly1="every 1 week at 12:00:00 do --pending -qqq"
+                JOB_weekly2="every monday at 12:00:00 do --pending -qqq"
+                
+                Supported grammar:
+                every [number] <second|minute|hour|day|week|seconds|minutes|...> [at <[[HH:]MM]:SS>] do <command>
+                every <day name> [at <[[HH:]MM]:SS>] do <command>
+                """
+        )
+    )
     p.add_argument("-v", "--verbose", action="count", default=0, help="Increase verbosity")
     p.add_argument("-q", "--quiet", action="count", default=0, help="Decrease verbosity")
 
